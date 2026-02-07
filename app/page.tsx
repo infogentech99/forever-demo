9ff140efe9dc84399e141976731da58e3bb60d2b
unknown <md.fardeen5236@gmail.com>
2026-02-07 18:51:17 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+export default function Home() {
+  return (
+    <>
+      <div
+        className=" bg-[url('https://res.cloudinary.com/dx2di0mvx/image/upload/v1770459026/forever_demo_bg_bo6huu.png')]
+    md:bg-[url('https://res.cloudinary.com/dx2di0mvx/image/upload/v1770459026/forever_demo_bg_bo6huu.png')]
+    bg-cover bg-no-repeat bg-top md:bg-center w-full px-4 sm:px-8  overflow-hidden relative">
+
+        <div className="pt-32 pb-50 relative z-10">
+
+          <h2 className="text-white  text-center leading-tight
+     text-[50px] sm:text-5xl lg:text-[80px] md:pb-0 pb-0
+  flex flex-col items-center gap-y-6">
+
+            <span className="font-playfair-display font-normal">I  Made <span className="italic"> Something</span>
+              <br /> For <span className="italic">You</span></span>
+            <span className="font-cormorant italic text-2xl">Lets rewind time for a little while</span>
+          </h2>
+
+
+
+
+          <div className="flex flex-col mt-530 mb-110 ml-[30%] -rotate-10 mr-[45%]  text-center">
+
+            <h2 className="text-[#841426] lg:text-[30px] md:text-2xl text-[18px]">
+              <span className="font-carattere text-cent">The</span>   <br />
+              <span className="font-dancing-script font-bold text-3xl"> BEGINNING</span>
+              <span className="font-carattere"> of</span> <br />
+              <span className="font-carattere text-5xl">Forever </span>
+              <span className="font-dancing-script font-bold">STARTS HERE</span>
+            </h2>
+
+            <h2 className="text-black font-cormorant-upright lg:text-[30px] md:text-2xl text-[22px]">
+              First Meet
+            </h2>
+          </div>
+
+
+          <div className="mt-20 mb-30">
+            <div className="flex flex-col mt-0 ml-[39%] mr-[38%]  text-center">
+
+              <h2 className="text-[#841426] lg:text-[30px] md:text-2xl text-[18px] ">
+                <span className="font-carattere text-cent">The</span><br/><span className="font-dancing-script font-bold text-3xl"> BEGINNING</span> 
+                <span className="font-carattere"> of</span> <br /> <span className="font-carattere text-5xl">Forever </span>
+                <span className="font-dancing-script font-bold">STARTS HERE</span>
+              </h2>
+
+              <h2 className="text-black font-cormorant-upright lg:text-[30px] md:text-2xl text-[22px]">
+                First Meet
+              </h2>
+            </div>
+          </div>
+
+
+<div className="mt-180 mb-0">
+            <div className="flex flex-col mt-0 ml-[24%] mr-[40%]  text-center">
+
+              <h2 className="text-[#841426] lg:text-[30px] md:text-2xl text-[18px] ">
+                <span className="font-carattere text-cent">The</span><br/><span className="font-dancing-script font-bold text-3xl"> BEGINNING</span> 
+                <span className="font-carattere"> of</span> <br /> <span className="font-carattere text-5xl">Forever </span>
+                <span className="font-dancing-script font-bold">STARTS HERE</span>
+              </h2>
+              <h2 className="text-black font-cormorant-upright lg:text-[30px] md:text-2xl text-[22px]">
+                First Meet
+              </h2>
+            </div>
+
+          </div>
+
+
+<div className="mt-240">
+            <div className="flex flex-col mt-0 ml-[32%] -rotate-16 mr-[40%] text-center">
+
+              <h2 className="text-[#841426] lg:text-[30px] md:text-2xl text-[18px] ">
+                <span className="font-carattere text-cent">The</span><br/><span className="font-dancing-script font-bold text-3xl"> BEGINNING</span> 
+                <span className="font-carattere"> of</span> <br /> <span className="font-carattere text-5xl">Forever </span>
+                <span className="font-dancing-script font-bold">STARTS HERE</span>
+              </h2>
+
+              <h2 className="text-black font-cormorant-upright lg:text-[30px] md:text-2xl text-[22px]">
+                First Meet
+              </h2>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </>
+  );
+}
